d17201687511a04788e74b436be0b66df0772ed7
ROMA <romanchep2006@gmail.com>
2025-02-27 15:15:57 +0700

    создан основной класс Document с свойствами имя/автор ключевые/слова/путь к файлу

--- program.cs
+++ program.cs
@@ -1,1 +1,20 @@
-/program
+using System;
+class Document {
+  public string Name { get; }
+  public string Author { get; }
+  public string Keywords { get; }
+  public string Topic { get; }
+  public string FilePath { get; }
+
+  public Document(string name, string author, string keywords, string topic, string filePath) {
+    Name = name;
+    Author = author;
+    Keywords = keywords;
+    Topic = topic;
+    FilePath = filePath;
+  }
+
+  public virtual void GetInfo() {
+    Console.WriteLine($"Название: {Name}, Автор: {Author}, Ключевые слова: {Keywords}, Тематика: {Topic}, Путь: {FilePath}");
+  }
+}
